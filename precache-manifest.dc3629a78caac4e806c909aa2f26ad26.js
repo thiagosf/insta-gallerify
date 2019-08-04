ab8150303b67c69d687e0c7922120d31e9f78d86
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-08-03 22:13:57 -0300

    Updates

---
+++ precache-manifest.dc3629a78caac4e806c909aa2f26ad26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abeb8973d3cdba02438767f5e72514d",
+    "url": "/insta-gallerify/index.html"
+  },
+  {
+    "revision": "5395d2b0ebc97e345da8",
+    "url": "/insta-gallerify/static/css/2.e8537005.chunk.css"
+  },
+  {
+    "revision": "b35daf71b5ae43ed548a",
+    "url": "/insta-gallerify/static/css/main.68d15467.chunk.css"
+  },
+  {
+    "revision": "5395d2b0ebc97e345da8",
+    "url": "/insta-gallerify/static/js/2.cc33c362.chunk.js"
+  },
+  {
+    "revision": "b35daf71b5ae43ed548a",
+    "url": "/insta-gallerify/static/js/main.ee943df6.chunk.js"
+  },
+  {
+    "revision": "c7faf968c87fada0bba1",
+    "url": "/insta-gallerify/static/js/runtime~main.678c0bb6.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/insta-gallerify/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/insta-gallerify/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/insta-gallerify/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/insta-gallerify/static/media/slick.f97e3bbf.svg"
+  }
+]);
